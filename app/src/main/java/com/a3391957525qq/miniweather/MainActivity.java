9e1c7dbd391d565fb80fc59bf14a0d183a6512a1
zsy <3391957525@qq.com>
2018-10-07 18:53:59 +0800

    hello

---
+++ app/src/main/java/com/a3391957525qq/miniweather/MainActivity.java
@@ -0,0 +1,16 @@
+package com.a3391957525qq.miniweather;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by oemyner on 2018/10/6.
+ */
+
+public class MainActivity extends Activity {
+        @Override
+        protected void onCreate(Bundle savedInstanceState){
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.weather_info);
+    }
+}
